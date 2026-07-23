0494816c8c9a3d7b04013a565047dcc509a90076
mapaspontexsagunto-collab <mapaspontexsagunto@gmail.com>
2026-07-23 21:29:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
    ========================================================= */
 
 const CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbyvRr2MskhM9DdZ6IeyYYWBXbxy_fFDgDzHjzbOCitJTQdRDnkBf8n4Zn8JLqFz3OEz-Q/exec", // <-- pega aquí la URL del Web App de Apps Script cuando la tengas
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwb3nsvGWHxr0jiFr4CtJAi2lPcTk8fdHEl1g1Xp7R15gV_ElLYwbDbd1BTaPx_KE3lwA/exec", // <-- pega aquí la URL del Web App de Apps Script cuando la tengas
   ADMIN_PIN: "1216", // PIN de demo. En producción la confirmación se hace desde el backend, no aquí.
   OWNER_WHATSAPP: "34629733085",
   OPEN_HOUR: 9,
